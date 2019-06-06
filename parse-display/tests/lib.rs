049985b88854abafc34c1fe7da4877e9b7433f39
frozenlib <frozenlib@users.noreply.github.com>
2019-06-06 21:20:49 +0900

    Remove ununsed code.

--- parse-display/tests/lib.rs
+++ parse-display/tests/lib.rs
@@ -1,10 +1,1 @@
-use parse_display::*;
-use std::fmt::Debug;
-use std::str::FromStr;
 
-// #[derive(FromStr, Debug, Eq, PartialEq)]
-// #[display("{a},{b}")]
-// struct TestStruct {
-//     a: u32,
-//     b: u32,
-// }
