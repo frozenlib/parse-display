ef79669dd919d6bfb1d3aa5342effa3f08793073
frozenlib <frozenlib@users.noreply.github.com>
2019-06-10 11:53:50 +0900

    Remove unused code.

--- parse-display/tests/lib.rs
+++ parse-display/tests/lib.rs
@@ -1,16 +1,1 @@
-use parse_display::*;
-use std::fmt::Display;
-
-    #[derive(Display)]
-    enum TestEnum {
-        #[display("{} = {x}")]
-        A {
-            #[display("---{l}")]
-            x: TestStruct,
-        },
-    }
-
-    struct TestStruct {
-        l: String,
-    }
 
