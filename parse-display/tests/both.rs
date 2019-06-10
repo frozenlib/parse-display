08bcbe494d0ef9238bf843b55f2903d26ca62195
frozenlib <frozenlib@users.noreply.github.com>
2019-06-10 13:22:48 +0900

    Add test for use both Display and FromStr.

---
+++ parse-display/tests/both.rs
@@ -0,0 +1,40 @@
+use parse_display::*;
+use std::fmt::Debug;
+use std::fmt::Display;
+use std::str::FromStr;
+
+
+#[test]
+fn both_newtype() {
+    #[derive(Display, FromStr, Debug, PartialEq)]
+    struct TestStruct(u32);
+
+    assert_both("12", TestStruct(12));
+
+}
+
+#[test]
+fn both_struct_format() {
+    #[derive(Display, FromStr, Debug, PartialEq)]
+    #[display("{a},{b}")]
+    struct TestStruct {
+        a: u32,
+        b: u32,
+    }
+    assert_both("12,50", TestStruct { a: 12, b: 50 });
+}
+
+
+fn assert_both<T: Display + FromStr + PartialEq + Debug>(s: &str, value: T)
+where
+    <T as FromStr>::Err: Display,
+{
+    let value_display = format!("{}", value);
+    assert_eq!(value_display, s);
+
+    match s.parse::<T>() {
+        Ok(a) => assert_eq!(a, value, "input = \"{}\"", s),
+        Err(e) => panic!("\"{}\" parse failed. ({})", s, e),
+    }
+
+}
