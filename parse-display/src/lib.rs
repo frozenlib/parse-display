b751d9db6eb483c0e0f4438ddaeca794d03cb4ed
frozenlib <frozenlib@users.noreply.github.com>
2019-06-11 09:31:33 +0900

    Remove serde dependency.

--- parse-display/src/lib.rs
+++ parse-display/src/lib.rs
@@ -1,8 +1,4 @@
-use serde::de::Deserializer;
-use std::fmt;
-use std::fmt::{Display, Formatter};
-use std::marker::PhantomData;
-use std::str::FromStr;
+use std::fmt::{Display, Formatter, Result};
 
 pub mod helpers {
     pub use lazy_static;
@@ -23,7 +19,7 @@ impl ParseError {
 }
 
 impl Display for ParseError {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut Formatter) -> Result {
         write!(f, "{}", self.0)
     }
 }
@@ -31,35 +27,4 @@ impl std::error::Error for ParseError {
     fn description(&self) -> &str {
         self.0
     }
-}
-
-
-pub fn deserialize_from_str<'de, D, T>(d: D) -> Result<T, D::Error>
-where
-    T: FromStr,
-    D: Deserializer<'de>,
-{
-    use serde::de::*;
-    struct StrVisitor<T> {
-        _phantom: PhantomData<fn() -> T>,
-    }
-    impl<'de, T: FromStr> Visitor<'de> for StrVisitor<T> {
-        type Value = T;
-        fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
-            write!(formatter, "string")
-        }
-        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: Error,
-        {
-            if let Ok(value) = FromStr::from_str(v) {
-                Ok(value)
-            } else {
-                Err(Error::invalid_value(Unexpected::Str(v), &self))
-            }
-        }
-    }
-    d.deserialize_str(StrVisitor {
-        _phantom: PhantomData,
-    })
 }
