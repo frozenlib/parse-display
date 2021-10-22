687218b3c4bd379d90aaa131f6fb4da98f2fc1c3
frozenlib <frozenlib@users.noreply.github.com>
2021-10-22 11:36:55 +0900

    Remove unnecessary trailing semicolons.

--- parse-display-derive/src/syn_utils.rs
+++ parse-display-derive/src/syn_utils.rs
@@ -14,13 +14,13 @@ use syn::{
 
 macro_rules! bail {
     ($span:expr, $message:literal $(,)?) => {
-        return std::result::Result::Err(syn::Error::new($span, $message));
+        return std::result::Result::Err(syn::Error::new($span, $message))
     };
     ($span:expr, $err:expr $(,)?) => {
-        return std::result::Result::Err(syn::Error::new($span, $err));
+        return std::result::Result::Err(syn::Error::new($span, $err))
     };
     ($span:expr, $fmt:expr, $($arg:tt)*) => {
-        return std::result::Result::Err(syn::Error::new($span, std::format!($fmt, $($arg)*)));
+        return std::result::Result::Err(syn::Error::new($span, std::format!($fmt, $($arg)*)))
     };
 }
 
