58bf84790be051a00c44dac76232e5150b1ce8bb
frozenlib <frozenlib@users.noreply.github.com>
2024-11-17 22:03:08 +0900

    Implement FromStrRegex for some types.

--- parse-display/src/from_str_regex.rs
+++ parse-display/src/from_str_regex.rs
@@ -1,3 +1,6 @@
+use core::num::NonZero;
+use std::{ffi::OsString, path::PathBuf};
+
 /// A trait for getting regex patterns that match strings parseable by [`FromStr`](core::str::FromStr).
 ///
 /// When using [`#[derive(FromStr)]`](derive@crate::Display) with the [`#[from_str(regex_infer)]`](derive@crate::Display#from_strregex_infer) attribute,
@@ -8,9 +11,30 @@ pub trait FromStrRegex: core::str::FromStr {
     /// Note: Matching this pattern does not guarantee that the string can be parsed successfully.
     fn from_str_regex() -> String;
 }
+
+impl FromStrRegex for char {
+    fn from_str_regex() -> String {
+        r"(?s:.)".into()
+    }
+}
+
+fn regex_any() -> String {
+    r"(?s:.*?)".into()
+}
+
 impl FromStrRegex for String {
     fn from_str_regex() -> String {
-        "(?s:.*?)".into()
+        regex_any()
+    }
+}
+impl FromStrRegex for OsString {
+    fn from_str_regex() -> String {
+        regex_any()
+    }
+}
+impl FromStrRegex for PathBuf {
+    fn from_str_regex() -> String {
+        regex_any()
     }
 }
 
@@ -28,22 +52,62 @@ impl FromStrRegex for u8 {
         regex_uint()
     }
 }
+impl FromStrRegex for NonZero<u8> {
+    fn from_str_regex() -> String {
+        regex_uint()
+    }
+}
+
 impl FromStrRegex for u16 {
     fn from_str_regex() -> String {
         regex_uint()
     }
 }
+impl FromStrRegex for NonZero<u16> {
+    fn from_str_regex() -> String {
+        regex_uint()
+    }
+}
+
 impl FromStrRegex for u32 {
     fn from_str_regex() -> String {
         regex_uint()
     }
 }
+impl FromStrRegex for NonZero<u32> {
+    fn from_str_regex() -> String {
+        regex_uint()
+    }
+}
+
 impl FromStrRegex for u64 {
     fn from_str_regex() -> String {
         regex_uint()
     }
 }
+impl FromStrRegex for NonZero<u64> {
+    fn from_str_regex() -> String {
+        regex_uint()
+    }
+}
+
 impl FromStrRegex for u128 {
+    fn from_str_regex() -> String {
+        regex_uint()
+    }
+}
+impl FromStrRegex for NonZero<u128> {
+    fn from_str_regex() -> String {
+        regex_uint()
+    }
+}
+
+impl FromStrRegex for usize {
+    fn from_str_regex() -> String {
+        regex_uint()
+    }
+}
+impl FromStrRegex for NonZero<usize> {
     fn from_str_regex() -> String {
         regex_uint()
     }
@@ -52,27 +116,62 @@ impl FromStrRegex for u128 {
 fn regex_sint() -> String {
     r"-?[0-9]+".into()
 }
+
 impl FromStrRegex for i8 {
     fn from_str_regex() -> String {
         regex_sint()
     }
 }
+impl FromStrRegex for NonZero<i8> {
+    fn from_str_regex() -> String {
+        regex_sint()
+    }
+}
+
 impl FromStrRegex for i16 {
     fn from_str_regex() -> String {
         regex_sint()
     }
 }
+impl FromStrRegex for NonZero<i16> {
+    fn from_str_regex() -> String {
+        regex_sint()
+    }
+}
+
 impl FromStrRegex for i32 {
     fn from_str_regex() -> String {
         regex_sint()
     }
 }
+impl FromStrRegex for NonZero<i32> {
+    fn from_str_regex() -> String {
+        regex_sint()
+    }
+}
+
 impl FromStrRegex for i64 {
     fn from_str_regex() -> String {
         regex_sint()
     }
 }
+impl FromStrRegex for NonZero<i64> {
+    fn from_str_regex() -> String {
+        regex_sint()
+    }
+}
 impl FromStrRegex for i128 {
+    fn from_str_regex() -> String {
+        regex_sint()
+    }
+}
+
+impl FromStrRegex for isize {
+    fn from_str_regex() -> String {
+        regex_sint()
+    }
+}
+impl FromStrRegex for NonZero<isize> {
     fn from_str_regex() -> String {
         regex_sint()
     }
